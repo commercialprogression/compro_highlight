3b6f079e20cc126bd5de3aaf8204e0c45b5544c3
Brad Czerniak <ao5357@gmail.com>
2015-07-10 12:20:14 -0400

    View modes and paragraph bundle

--- highlight--standard.tpl.php
+++ highlight--standard.tpl.php
@@ -2,13 +2,6 @@
   <div class="content"<?php print $content_attributes; ?>>
     <?php print render($content['field_highlight_media']); ?>
     <div class="highlight-standard-field-wrapper">
-      <h2<?php print $title_attributes; ?>>
-        <?php if ($url): ?>
-          <a href="<?php print $url; ?>"><?php print $title; ?></a>
-        <?php else: ?>
-          <?php print $title; ?>
-        <?php endif; ?>
-      </h2>
       <?php print render($content); ?>
     </div>
   </div>
